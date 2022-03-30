fd187ade58facfd5ab0221cadcb768a2d78c8326
S1bery <45617892+S1bery@users.noreply.github.com>
2022-03-30 17:26:43 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<float> V(4);
+    std::vector<float> R(4);
+    float M[4][4];
+
+    std::cout << "V:" << std::endl;
+    for (int i = 0; i < 4; i++) {
+            std::cin >> V[i];
+    }
+
+    std::cout << "M:" << std::endl;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cin >> M[i][j];
+        }
+    }
+
+    for(int i = 0; i < 4; i++){
+        float sum = 0;
+        for (int j = 0; j < 4; j++) {
+            sum += M[i][j] * V[i];
+        }
+        R[i] = sum;
+    }
+    std::cout << "R: " << std::endl;
+    for (int i = 0; i < 4; i++) {
+        std::cout << R[i] << " ";
+    }
+
+    return 0;
+}
